4691c35aa3f2cdb4a80a0e9b052a9a85b4507cc3
Ang3l1llo <angelsanchezmendoz@gmail.com>
2025-07-07 14:54:40 +0200

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Ang3l1llo.github.io',
+  base: 'Ang3l1llo.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
